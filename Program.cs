55a6bca8642499d792055e793206eac19b6199e1
Cleydson Fernando <cleydsonfernando@gmail.com>
2022-04-29 15:28:52 -0300

    Jogo_Adivinhar_Número

---
+++ Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace Jogo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sorteio = new Random().Next(1, 5);
+            
+            int tentativas = 0;
+            string resp = "s";
+            while (resp == "s")
+            {
+
+                Console.WriteLine("************************ JOGO DE ADIVINHAÇÃO **********************************");
+                Console.WriteLine();
+                Console.WriteLine("Tente adivinhar o número que eu pensei entre 1 e 5: ");
+                int chute = int.Parse(Console.ReadLine());
+
+                
+
+                if (sorteio == chute)
+                {
+                    Console.WriteLine("Pensei em: {0}", sorteio);
+                    Console.WriteLine("Parabéns você acertou!!! ");
+                    {
+                        tentativas++;
+                        Console.WriteLine("Você tentou {0} vezes! ", tentativas);
+                        Console.WriteLine("Fim do jogo!");
+                        break;
+                    }
+                }
+
+                else
+                {
+                    if (chute == 1)
+                    {
+                        Console.WriteLine($"Errou, pensei em :  {sorteio}");
+                        Console.WriteLine("Continuar?:  (s/n)");
+                        resp = Console.ReadLine();
+                        tentativas++;
+                        Console.WriteLine("Você tentou {0} vezes! ", tentativas);
+                        Console.Clear();
+
+                    }
+                    else if (chute == 2)
+                    {
+                        Console.WriteLine($"Não foi desta vez, eu pensei em :  {sorteio}");
+                        Console.WriteLine("Continuar?:  (s/n)");
+                        resp = Console.ReadLine();
+                       
+                    }
+
+                    else if (chute == 3)
+                    {
+                        Console.WriteLine($"Vamos lá sei que você consegue, mas dessa vez eu pensei em :  {sorteio}");
+                        Console.WriteLine("Continuar?:  (s/n)");
+                        resp = Console.ReadLine();
+                       
+                    }
+                    else if (chute == 4)
+                    {
+                        Console.WriteLine($"Por pouco.. eu pensei desta vez em :  {sorteio}");
+                        Console.WriteLine("Continuar?:  (s/n)");
+                        resp = Console.ReadLine();
+                        
+                    }
+
+                    else
+                    {
+                        Console.WriteLine($"Quase... tenta mais uma ai, eu pensei em:  {sorteio}");
+                        Console.WriteLine("Continuar?:  (s/n)");
+                        resp = Console.ReadLine();
+                       
+                    }
+
+                    tentativas++;
+                    Console.WriteLine("Você tentou {0} vezes! ", tentativas);
+                    Console.Clear();
+                }
+
+
+                
+                    Console.Clear();
+                    Console.WriteLine("Fim do Programa");
+                
+
+
+
+            }
+        }
+    }
+}
